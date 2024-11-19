9982a00422e677d49b84d06aaa72525ce02d7a97
Santiago Salcedo <ssalcedor@unal.edu.co>
2024-11-19 10:45:51 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -10,37 +10,37 @@ class Control:
     self._tv = tv
 
   def turnOn(self):
-    if self.tv.estado :
+    if self._tv.estado :
       self._tv.turnOn()
   
   def turnOff(self):
-   if self.tv.estado :
+   if self._tv.estado :
     self._tv.turnOff()
   
   def canalUp(self):
-   if self.tv.estado :
+   if self._tv.estado :
     self._tv.canalUp()
   
   def canalDown(self):
-   if self.tv.estado :
+   if self._tv.estado :
     self._tv.canalDown()
   
   def volumenUp(self):
-    if self.tv.estado :
+    if self._tv.estado :
      self._tv.volumenUp()
   
   def volumenDown(self):
-   if self.tv.estado :
+   if self._tv.estado :
     self._tv.volumenDown()
   
-  def setCanal(self):
-   if self.tv.estado :
-    self._tv.setCanal()
+  def setCanal(self,canal):
+   if self._tv.estado :
+    self._tv.setCanal(canal)
   
-  def setVolumen(self):
-    if self.tv.estado :
-     self._tv.setVolumen()
+  def setVolumen(self,canal):
+    if self._tv.estado :
+     self._tv.setVolumen(canal)
 
   def enlazar(self,tv):
-    self.tv = tv
-    self.tv.setControl(self)
+    self._tv = tv
+    self._tv.setControl(self)
